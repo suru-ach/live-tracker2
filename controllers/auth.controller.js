58faa0f379c62698ad5587e2ba1ad8b3917f7a01
suru-ach <srujanacharya36@gmail.com>
2023-11-10 10:59:42 +0530

    skill issue

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,19 +1,41 @@
+const { Pool } = require('pg');
+
+const clientInstance = new Pool({
+    connectionString: process.env.CONNECTION_STRING_PG
+});
+
 const signup = (req, res) => {
     const { username, password, phonenumber } = req.body;
 
     if(!username || !password || !phonenumber) {
         return res.status(401).json({ data: "invalid details" });
     }
+
     return res.status(201).json({ data: req.body });
 }
 
-const login = (req, res) => {
+const login = async (req, res) => {
     const { username, password } = req.body;
 
     if(!username || !password) {
         return res.status(401).json({ data: "unauthorized, invalid username or password" });
     }
-    return res.status(201).json({ data: req.body });
+
+    const queryText = 'select * from "user"';
+    let client;
+
+    try {
+        client = await clientInstance.connect();
+        const result = await clientInstance.query(queryText);
+        console.log(result.rows);
+        return res.status(201).json({ data: result.rows[0] });
+    }
+    catch(err) {
+        return res.status(500).json({ data: err });
+    }
+    finally {
+        if(client) client.release();    
+    }
 }
 
 const signout = (req, res) => {
@@ -25,3 +47,5 @@ module.exports = {
     login,
     signout
 }
+
+
