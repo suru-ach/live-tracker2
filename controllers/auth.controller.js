3e988f9a97b1764c896efd3636e6e350f4fb6d3e
suru-ach <srujanacharya36@gmail.com>
2023-11-10 13:18:19 +0530

    ssl

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,10 @@
 const { Pool } = require('pg');
 
 const clientInstance = new Pool({
-    connectionString: process.env.CONNECTION_STRING_PG
+    connectionString: process.env.CONNECTION_STRING_PG,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const signup = (req, res) => {
