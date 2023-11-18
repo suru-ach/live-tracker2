a77f55422145971be1c3435bef82e19fb618582a
suru-ach <srujanacharya36@gmail.com>
2023-11-18 12:32:03 +0530

    test2 coords

--- controllers/coords.controller.js
+++ controllers/coords.controller.js
@@ -1,9 +1,34 @@
-const getcoords = (req, res) => {
-    return res.status(200).json({ data: 'yes' });    
+const { Pool } = require('pg');
+const clientInstance = new Pool({
+    connectionString: process.env.CONNECTION_STRING_PG,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+const getcoords = async (req, res) => {
+    try {
+        const { rid } = req.body;
+        const result = await clientInstance.query('select * from coords where rid = $1', [rid]);
+        const data = result.rows;
+        return res.status(200).json({ data });    
+    }
+    catch(err) {
+        return res.status(400).json({ data: err });
+    }
 }
 
-const updatecoords = (req, res) => {
-    return res.status(200).json({ data: req.body });
+const updatecoords = async (req, res) => {
+    try {
+        const { rid, x, y , count } = req.body;
+        const queryText = 'insert into coords (rid, x, y, ping_no) values ($1, $2, $3, $4)';
+        const queryParams = [rid, x, y, count];
+        const updatedresult = await clientInstance.query(queryText, queryParams);
+        return res.status(201).json({ data: updatedresult });
+    }
+    catch(err) {
+        return res.status(400).json({ data: err });
+    }
 }
 
 module.exports = {
