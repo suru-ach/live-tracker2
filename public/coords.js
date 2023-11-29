e5b26746224cbbc32d7f133d6630ba2ab1c04dbe
suru-ach <srujanacharya36@gmail.com>
2023-11-29 20:52:20 +0530

    add coords-friends

--- public/coords.js
+++ public/coords.js
@@ -1,3 +1,5 @@
+const friendsList = document.getElementById('friends');
+
 const createRoomButton = document.getElementById('create-room');
 const terminateRoomButton = document.getElementById('terminate-room');
 const sendCoords = document.getElementById('tick');
@@ -109,3 +111,46 @@ async function updateCoords()
         .then((res) => console.log(res))
         .catch((err) => console.log(err));
 }
+
+async function selectToRoom(e) {
+    console.log(e);
+}
+
+function populateFriendsList(data) {
+    const friendsHtmlList = data
+        .filter(user => user.isfriend === true)
+        .map(({ username, phonenumber }) => {
+            return `
+                <li class="contact">
+                    <div class="contact-img"></div>
+                    <div>
+                        <h2>${username}</h2>
+                        <p>${phonenumber}</p>
+                    </div>
+                    <div class="button delete-button" data-phonenumber=${phonenumber}>
+                        remove
+                    </div>
+                </li>
+            `;
+        })
+        .join('');
+
+    friendsList.innerHTML = friendsHtmlList ? friendsHtmlList : '<h4>no friends ;( </h4>' ;
+}
+
+async function getFriends() {
+    friendsList.innerHTML = '<h4>loading...</h4>' ;
+    await fetch('/getfriends', {
+        method: 'GET',
+        credentials: 'include',
+    })
+        .then((res) => {
+            console.log(res);
+            return res.json();
+        })
+        .then((data) => populateFriendsList(data.data))
+        .then(err => console.log(err))
+}
+
+friendsList.addEventListener('click', e => selectToRoom(e));
+getFriends();
